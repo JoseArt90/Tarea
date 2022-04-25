006e9d4c8616eac471b2f4cbdd96b1cd0fba3e37
JoseArt90 <jos_oswald@hotmail.com>
2022-04-25 00:07:06 -0500

    tarea

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = "";
+  titulo = 'Gaming Tech&Systems';
+  foto = 'C:\Users\P3P3\Desktop\angular\appdemo\src\assets\logo.png'
+  setup = 'C:\Users\P3P3\Desktop\angular\appdemo\src\assets\setup.PNG'
+  setup1 = 'C:\Users\P3P3\Desktop\angular\appdemo\src\assets\setup1.PNG'
+  setup2 = 'C:\Users\P3P3\Desktop\angular\appdemo\src\assets\setup2.PNG'
+
+  }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { FrameComponent } from './frame/frame.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    FrameComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
